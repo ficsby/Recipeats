78e6b9ed06285f87d53743de2e2e9db7886c2a8c
Francis Buendia <francisgbuendia@gmail.com>
2019-04-24 16:22:00 -0700

    merged master with userrecipes-screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,16 +66,7 @@ export default class HomeScreen extends React.Component {
      };
 
     onAccountIconPress = () => {
-        // var navActions = StackActions.reset({
-        //     index: 1,
-        //     actions: [
-        //         // We need to push both the current screen and the next screen that we are transitioning to incase the user wants to go to previous screen
-        //         StackActions.push({ routeName: "Home" }),       
-        //         StackActions.push({ routeName: "EditAccount" }),
-        //     ]
-        // });
-
-        // this.props.navigation.dispatch(navActions);
+        NavigationService.navigate('EditAccountScreen');
         this.setState({visible: true});
     };
 
